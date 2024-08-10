18714ee63535840e95ee1f8d4bada045fd9d3e1c
Homayoun <homayoun.srp@gmail.com>
2024-08-10 01:30:06 -0400

    main file function updated

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
 from graphRAG_generation import GraphRAG
 from vectorRAG_generation import VectorRAG
 
-def query_graph_or_vector_rag(use_relationship: bool, question: str) -> str:
-    if use_relationship:
+
+def query_graph_or_vector_rag(use_graph: bool, question: str) -> str:
+    if use_graph:
         # GraphRAG
         query_generator = GraphRAG()
         cypher_query = query_generator.generate_cypher_query(question)
@@ -14,13 +15,15 @@ def query_graph_or_vector_rag(use_relationship: bool, question: str) -> str:
         answer = retrieval_qa.query(question)
         return f"Answer: {answer}"
 
+
 # query
 question = "Who was leading the Battle of Waterloo?"
 
-# Use GraphRAG
+# Use GraphRAG -> True
+# Use VectorRAG -> False
 result_relationship = query_graph_or_vector_rag(True, question)
 print(result_relationship)
 
-# Use VectorRAG
-result_vector = query_graph_or_vector_rag(False, question)
-print(result_vector)
+# # Use VectorRAG
+# result_vector = query_graph_or_vector_rag(False, question)
+# print(result_vector)
